b12349b13141ac91290808d0bd44f41ce9262ee5
Rafael L <118671591+rafaellopesss@users.noreply.github.com>
2023-12-28 23:41:01 -0300

    save

--- tela-login/javascript/animacao.js
+++ tela-login/javascript/animacao.js
@@ -1,10 +1,2 @@
-function mudarcorinput() {
-    var input = document.getElementById('email')
-    
-    if (input.style.backgroundColor == 'cyan'){
-        input.style.backgroundColor = 'white'
-    }else {
-        input.style.backgroundColor = 'cyan'
-    }
 
-}
+
